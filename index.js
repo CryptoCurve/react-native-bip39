c02173e4d01e74c909a33b4489a27ebce747231c
jasonsheldon <jason.sheldon@trintel.co.za>
2019-03-19 15:49:07 +0200

    try use a local randomBytes using in built into node crypto.getRandomValues

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ var unorm = require('unorm')
 var assert = require('assert')
 var pbkdf2 = require('react-native-crypto').pbkdf2Sync
 var createHash = require('react-native-crypto').createHash
-var randomBytes = require('react-native-randombytes').randomBytes
+
 
 var DEFAULT_WORDLIST = require('./wordlists/en.json')
+var MAX_BYTES = 65536
 
+// Node supports requesting up to this number of bytes
+// https://github.com/nodejs/node/blob/master/lib/internal/crypto/random.js#L48
+var MAX_UINT32 = 4294967295
+
+function oldBrowser () {
+  throw new Error('Secure random number generation is not supported by this browser.\nUse Chrome, Firefox or Internet Explorer 11')
+}
+
+var Buffer = require('safe-buffer').Buffer
 function mnemonicToSeed(mnemonic, password) {
   var mnemonicBuffer = new Buffer(mnemonic, 'utf8')
   var saltBuffer = new Buffer(salt(password), 'utf8')
@@ -69,6 +79,36 @@ function entropyToMnemonic(entropy, wordlist) {
   })
 
   return words.join(' ')
+}
+
+function randomBytes (size, cb) {
+
+  
+  // phantomjs needs to throw
+  if (size > MAX_UINT32) throw new RangeError('requested too many random bytes')
+
+  var bytes = Buffer.allocUnsafe(size)
+
+  if (size > 0) {  // getRandomValues fails on IE if size == 0
+    if (size > MAX_BYTES) { // this is the max bytes crypto.getRandomValues
+      // can do at once see https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
+      for (var generated = 0; generated < size; generated += MAX_BYTES) {
+        // buffer.slice automatically checks if the end is past the end of
+        // the buffer so we don't have to here
+        crypto.getRandomValues(bytes.slice(generated, generated + MAX_BYTES))
+      }
+    } else {
+      crypto.getRandomValues(bytes)
+    }
+  }
+
+  if (typeof cb === 'function') {
+    return process.nextTick(function () {
+      cb(null, bytes)
+    })
+  }
+
+  return bytes
 }
 
 function generateMnemonic(strength, rng, wordlist) {
